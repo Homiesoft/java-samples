c0d10519737f352b326112d0f8cc89fe3d7f969c
homefriend <jechin10@gmail.com>
2017-08-03 14:59:55 -0700

    Changed cells to small squares.

--- src/main/java/ColorSwapDemo.java
+++ src/main/java/ColorSwapDemo.java
@@ -9,12 +9,16 @@ public class ColorSwapDemo extends BaseForm{
     private JPanel panelMain;
     private JPanel panelCtrl;
     private JTable tblMain;
-    private JButton btnNew;
+    private JButton btnRestart;
     private JButton btnStep;
+    private JButton btnExit;
+    private JPanel panelDraw;
     private Color[] colors;
     //private Color[][] data;
     private final int ROWS = 9;
     private final int COLS = 9;
+    private int phase;
+    //private int[][] data;
     private int[][] marked;
 
     public ColorSwapDemo(){
@@ -23,23 +27,21 @@ public class ColorSwapDemo extends BaseForm{
         btnStep.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                step();
             }
         });
-        btnNew.addActionListener(new ActionListener() {
+        btnRestart.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 createBoard();
             }
         });
-    }
-
-    public void init() {
-        JFrame frame = new JFrame("ColorSwapDemo");
-        frame.setContentPane(new ColorSwapDemo().panelMain);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        btnExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.getDefaultCloseOperation();
+            }
+        });
     }
 
     private void createBoard(){
@@ -50,22 +52,39 @@ public class ColorSwapDemo extends BaseForm{
         Random rand = new Random();
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
-                Color c = colors[rand.nextInt(colors.length)];
-                tblMain.setValueAt(c, i, j);
+                tblMain.setValueAt(rand.nextInt(colors.length), i, j);
+                marked[i][j] = -1;
             }
         }
         //tblMain.setModel(new DefaultTableModel(data,null));
     }
+    private void step(){
+
+    }
+
+    public void init() {
+        JFrame frame = new JFrame("Color Swap");
+        frame.setContentPane(new ColorSwapDemo().panelMain);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        centerFrame();
+        phase = 0;
+    }
+
     private void createUIComponents() {
         // TODO: place custom component creation code here
+        //data = new int[ROWS][COLS];
+        marked = new int[ROWS][COLS];
         tblMain = new JTable(ROWS, COLS){
             @Override
             public Component prepareRenderer(TableCellRenderer renderer, int row, int col) {
                 Component comp = super.prepareRenderer(renderer, row, col);
                 Object value = getModel().getValueAt(row, col);
-                if (value instanceof Color) {
-                    comp.setBackground((Color)value);
-                    comp.setForeground((Color)value);
+                int num = (int)value;
+                if(num > -1 && num < colors.length){
+                    comp.setBackground(colors[num]);
+                    comp.setForeground(colors[num]);
                 }
                 return comp;
             }
@@ -75,8 +94,6 @@ public class ColorSwapDemo extends BaseForm{
         tblMain.setRowSelectionAllowed(false);
         tblMain.setSelectionBackground(Color.cyan);
         tblMain.setTableHeader(null);
-        //tblMain.setDefaultRenderer(Color.class, new CellRenderer());
-        marked = new int[ROWS][COLS];
         createBoard();
     }
 
